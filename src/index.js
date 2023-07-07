8a8f1a137da22b21bdde70389a8bc80bbfb6786f
william castro <wcastroh07@gmail.com>
2023-07-06 21:37:27 -0500

    conf routes add

--- src/index.js
+++ src/index.js
@@ -11,11 +11,31 @@ app.get("/",(req,res)=>{
     })
 })
 
+app.get("/api",(req,res)=>{
+    res.json({
+        routes:{
+            index:"/api/productos",
+            productos: [
+                "/api/productos/enlatados",
+                "/api/productos/enlatados/e001",
+                "/api/productos/lacteos",
+                "/api/productos/lacteos/l001"
+            ]
+        }
+    })
+})
+
 app.get("/api/productos",listarProductos)
 app.get("/api/productos/enlatados",listarEnlatados)
 app.get("/api/productos/enlatados/:codigo",getEnlatado)
 app.get("/api/productos/lacteos",listarLacteos)
 app.get("/api/productos/lacteos/:codigo",getLacteo)
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message:"Endpoint not found"
+    })
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
